860f7d64b83452e811aa95198103f3d3afe1a106
DESHPANDE NIKITA NANDKUMAR <nikita.deshpande1@walchandsangli.ac.in>
2023-12-15 13:08:30 +0530

    even_odd file added

---
+++ even_odd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    cout<<"Hey there lets add two numbers"<<endl;
+    cout<<"give me one numbers"<<endl;
+    int a;
+    cin>>a;
+    if(a%2==1){
+        cout<<"Odd number it is!"<<endl;
+    }
+    else{
+        cout<<"Even number it is!"<<endl;
+    }
+    return 0;
+}
