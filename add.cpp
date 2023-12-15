d55d10b263cd959fb343f88ef49705388c5b3e1b
DESHPANDE NIKITA NANDKUMAR <nikita.deshpande1@walchandsangli.ac.in>
2023-12-15 13:01:44 +0530

    new file added

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    cout<<"Hey there lets add two numbers"<<endl;
+    cout<<"give me two numbers"<<endl;
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+}
